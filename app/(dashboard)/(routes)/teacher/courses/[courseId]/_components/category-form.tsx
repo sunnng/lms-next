08de788c7ff0bf49a818a4b2c0e834619f8e65c2
sunnng <61936492+sunnng@users.noreply.github.com>
2023-12-02 16:20:27 +0800

    fix bug

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
@@ -108,7 +108,7 @@ export const CategoryForm = ({
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Combobox options={...options} {...field} />
+                    <Combobox options={[...options]} {...field} />
                   </FormControl>
                 </FormItem>
               )}
